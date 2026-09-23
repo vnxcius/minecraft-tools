649ecd640de877c02ee78d22b4d8f8f0f133431f
vnxcius <vncius@proton.me>
2026-09-23 18:09:45 -0300

    chore: remove Next.js and Cloudflare setup

--- next-sitemap.config.ts
+++
@@ -1,9 +0,0 @@
-import type { IConfig } from "next-sitemap";
-
-const config: IConfig = {
-	siteUrl: process.env.SITE_URL || "http://localhost:3000",
-	generateRobotsTxt: true,
-	sitemapSize: 7000,
-};
-
-export default config;
